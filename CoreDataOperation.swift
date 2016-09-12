085626b2fe6b8ef6529eabb8b35c8e41601b4ea8
Patrick Gao <patrickgao0922@gmail.com>
2016-09-12 17:29:30 +1000

    AddFunctionsForSaveManagedObjectContextAndGetLatestPosts
    Class CoreDataOperation
    func saveMangeObjectContext
    
    Class WordPressClient
    func getLatestPosts

--- CoreDataOperation.swift
+++ CoreDataOperation.swift
@@ -70,7 +70,17 @@ class CoreDataOperation {
         return nil
         
     }
+    
+    static func saveManagedObjectContext() {
+        let managedObjectContext = (UIApplication.sharedApplication().delegate as! AppDelegate).managedObjectContext
+        do{
+            try managedObjectContext.save()
+        } catch {
+            print("Errors when save Core Data managedObjectContext!!")
+        }
+    }
 }
+
 
 
 enum EntityType:String {
