52ef111d88f447f6638b5833a5c3b417dd245d8e
Patrick Gao <patrickgao0922@gmail.com>
2016-09-13 17:26:32 +1000

    AddResourcesTableViewController

---
+++ SubjectResourcesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SubjectResourcesTableViewCell.swift
+//  AIL
+//
+//  Created by Work on 13/09/2016.
+//  Copyright © 2016 au.com.melmel. All rights reserved.
+//
+
+import UIKit
+
+class SubjectResourcesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
