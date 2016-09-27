3b366264f07330b673a00c3fe0cefb39ed54d213
KaiP-598 <kaipengwu598@live.cn>
2016-09-28 01:43:20 +1000

    DoingUserMessages

---
+++ Message+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Message+CoreDataClass.swift
+//  AIL
+//
+//  Created by WuKaipeng on 28/09/2016.
+//  Copyright © 2016 au.com.melmel. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class Message: NSManagedObject {
+
+}
