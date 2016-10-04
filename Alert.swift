9e57a7f3fe11000ff62a7fd0defcd90252a9d0fa
KaiP-598 <kaipengwu598@live.cn>
2016-10-04 23:10:55 +1100

    AlertUpdateSecond

--- Alert.swift
+++ Alert.swift
@@ -57,7 +57,7 @@ class Alert {
         alertContentView.layer.cornerRadius = 10
         
         
-        let delay = 2.0 * Double(NSEC_PER_SEC)
+        let delay = 1.0 * Double(NSEC_PER_SEC)
         let time = dispatch_time(DISPATCH_TIME_NOW, Int64(delay))
         dispatch_after(time, dispatch_get_main_queue(), {
             alertController.dismissViewControllerAnimated(true, completion: nil)
