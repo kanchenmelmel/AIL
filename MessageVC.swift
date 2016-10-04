06d6bab3a5edbdc123685147eab79775c1574a7f
KaiP-598 <kaipengwu598@live.cn>
2016-10-04 21:26:23 +1100

    MessageVC

---
+++ MessageVC.swift
@@ -0,0 +1,40 @@
+//
+//  MessageVC.swift
+//  AIL
+//
+//  Created by WuKaipeng on 4/10/2016.
+//  Copyright © 2016 au.com.melmel. All rights reserved.
+//
+
+import UIKit
+
+class MessageVC: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+    var message:Message?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        titleLabel.text = message?.title
+        subtitleLabel.text = message?.content
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
